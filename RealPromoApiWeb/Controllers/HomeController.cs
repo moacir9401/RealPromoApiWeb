6a033bb106a73aea7fa8e249db8f78b56e86eee0
moacir <moacir9401@gmail.com>
2023-11-17 23:36:05 -0300

    criando telas home e instalando biblioteca

--- RealPromoApiWeb/Controllers/HomeController.cs
+++ RealPromoApiWeb/Controllers/HomeController.cs
@@ -6,27 +6,19 @@ namespace RealPromoApiWeb.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+       
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Promocao()
+        {
+            return View();
+        }
+        public IActionResult CadastrarPromocao()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
